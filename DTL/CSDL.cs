f74f8c3948e2b51a3f095d708815e0ecef8aee18
banghia <banghia.violet@gmail.com>
2018-05-29 17:58:30 +0700

    a

--- DTL/CSDL.cs
+++ DTL/CSDL.cs
@@ -17,9 +17,29 @@ namespace DTL
             DataTable dt = new DataTable();
             //Đối tượng SqlConnection sẽ tự động được giải phóng khi rời khỏi khối using.
             using (SqlConnection sqlConnection = new SqlConnection(chuoiKetNoi)) {
-                cmd.Connection = sqlConnection;
+                sqlConnection.Open(); //Mở kết nối cơ sở dữ liệu.
+                cmd.Connection = sqlConnection; //Gán kết nối csdl hiện tại cho SqlCommand.
+                SqlDataAdapter dap = new SqlDataAdapter(cmd);
+                dap.Fill(dt); //Đổ dữ liệu query từ csdl vào DataTable.
+                sqlConnection.Close(); //Đóng kết nối csdl.
             }
             return dt;
+        }
+
+        //Hàm thực thi câu lệnh truy vấn không phải select trả về số dòng thực hiện thành công.
+        //Câu lệnh truy vần non query: insert, update, delete,..
+        public int ExecuteNonQuery(SqlCommand cmd)
+        {
+            int result = 0;
+            //Đối tượng SqlConnection sẽ tự động được giải phóng khi rời khỏi khối using.
+            using (SqlConnection sqlConnection = new SqlConnection(chuoiKetNoi))
+            {
+                sqlConnection.Open(); //Mở kết nối cơ sở dữ liệu.
+                cmd.Connection = sqlConnection; //Gán kết nối csdl hiện tại cho SqlCommand.
+                result = cmd.ExecuteNonQuery(); //Thực thi câu lệnh truy vẫn NonQuery.
+                sqlConnection.Close(); //Đóng kết nối csdl.
+            }
+            return result;
         }
     }
 }
---
+++ DTL/MonHocDTL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using DTO;
+
+namespace DTL
+{
+    public class MonHocDTL
+    {
+        CSDL csdl = new CSDL();
+
+        //Trả về tất cả dữ liệu trong bảng MonHoc.
+        public DataTable All() {
+            string query = "select * from MonHoc";
+            SqlCommand cmd = new SqlCommand(query);
+            return csdl.ExecuteQuery(cmd);
+        }
+
+        //Trả về dữ liệu trong bảng MonHoc theo mã môn học.
+        public DataTable Get(string MaMonHoc) {
+            string query = "select * from MonHoc where MaMonHoc = @MaMonHoc";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("@MaMonHoc", MaMonHoc);
+            return csdl.ExecuteQuery(cmd);
+        }
+
+        public int Insert(MonHoc mh) {
+            string query = "insert into MonHoc values(@MaMonHoc, @TenMonHoc, @SoTiet)";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("@MaMonHoc", mh.MaMon);
+            cmd.Parameters.AddWithValue("@TenMonHoc", mh.TenMon);
+            cmd.Parameters.AddWithValue("@SoTiet", mh.SoTiet);
+            return csdl.ExecuteNonQuery(cmd);
+        }
+    }
+}
